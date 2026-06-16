fe8e7dc30eedce545c0116222d839b1d7ff5a89e
Dave Wheeler <dwheelerau@gmail.com>
2026-06-16 14:55:31 +1000

    busco update

--- scripts/4.busco.sh
+++ scripts/4.busco.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-- i input sequences or folder
-- m analyssis mode ie 'transcriptome'
-- c cores/threads
--l location of lineage dataset to buse used
--o outdir
---download "arthropoda_odb12.2" #metazoa_odb12.2
---download_path busco-db
+##
+#-i input sequences or folder
+#-m analyssis mode ie 'transcriptome'
+#-c cores/threads
+#-l "arthropoda_odb12.2" #metazoa_odb12.2
+#-o outdir_path
+## not required as will specify using l
+#--download "arthropoda_odb12.2" #metazoa_odb12.2
+#--download_path busco-db
+##
+busco -i transcripts.fa -m transcriptome -l "arthropoda_odb12.2" -o busco_out
